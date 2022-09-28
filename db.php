bba8e2bcab8c749411358f1507a68a031a434d04
yashgupta1299 <86516989+yashgupta1299@users.noreply.github.com>
2022-09-29 04:37:18 +0530

    add remote mysql connection

--- db.php
+++ db.php
@@ -4,7 +4,12 @@ protected $connection;
 
 function setconnection(){
     try{
-        $this->connection=new PDO("mysql:host=localhost; dbname=library_managment","root","");
+        // Development connection
+        // $this->connection=new PDO("mysql:host=localhost; dbname=library_managment","root","");
+
+        // Remote database connection
+        $this->connection=new PDO("mysql:host=remotemysql.com; dbname=Egqi5VWYI2","Egqi5VWYI2","8782gwAt1W");
+        
         // echo "Done";
     }catch(PDOException $e){
         echo "Error";
