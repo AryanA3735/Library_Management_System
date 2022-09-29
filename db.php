79242c4b67ba30fd9bfc16e1f7de2a314531edfb
yashgupta1299 <86516989+yashgupta1299@users.noreply.github.com>
2022-09-29 17:46:39 +0530

    update file

--- db.php
+++ db.php
@@ -8,11 +8,11 @@ function setconnection(){
         // $this->connection=new PDO("mysql:host=localhost; dbname=library_managment","root","");
 
         // Remote database connection
-        // $this->connection=new PDO("mysql:host=remotemysql.com; dbname=Egqi5VWYI2","Egqi5VWYI2","8782gwAt1W");
+        $this->connection=new PDO("mysql:host=remotemysql.com; dbname=Egqi5VWYI2","Egqi5VWYI2","8782gwAt1W");
         
         // include("/config/config.php");
-        include("/config/config.php");
-        $this->connection=new PDO("mysql:host=".HOSTNAME."; dbname=".DBNAME,USER,PASSWORD);
+        // include("config/config.php");
+        // $this->connection=new PDO("mysql:host=".HOSTNAME."; dbname=".DBNAME,USER,PASSWORD);
         
         // echo "Done";
     }catch(PDOException $e){
